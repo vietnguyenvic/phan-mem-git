519f68ead9dca8b44bab1f725c2de233e992af47
VIET <VIET@DESKTOP-JH8NPO8>
2021-01-06 20:11:34 +0700

    da tinh hieu

--- git.cpp
+++ git.cpp
@@ -4,10 +4,12 @@ int main()
 {
   int a=0,b=0;
   int  tong = 0;
+  int hieu=0;
   cout<<"nhap a";
   cin>>a;
   cout<<"nhap b";
   cin>>b;
   cout<<"tong la"<<tong;                   //tinh tong 
+  cout<<"hieu la"<<hieu;                   //tinh hieu
     return 0;
 }
